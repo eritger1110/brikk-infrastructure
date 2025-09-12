cd5baab31f61df19c59cf35404c592e0d3c9539c
eritger1110 <eric.ritger@gmail.com>
2025-09-12 15:07:10 -0500

    Import db and update address_json comment

--- src/models/customer_profile.py
+++ src/models/customer_profile.py
@@ -1,4 +1,5 @@
 # src/models/customer_profile.py
+from src.database.db import db
 
 class CustomerProfile(db.Model):
     __tablename__ = 'customer_profiles'
@@ -6,7 +7,7 @@ class CustomerProfile(db.Model):
     email = db.Column(db.String(255), unique=True, nullable=False, index=True)
     name = db.Column(db.String(255))
     phone = db.Column(db.String(50))
-    address_json = db.Column(db.Text)  # JSON dump of billing address
+    address_json = db.Column(db.Text)
     stripe_customer_id = db.Column(db.String(64), unique=True)
     subscription_id = db.Column(db.String(64), unique=True)
     plan = db.Column(db.String(128))
