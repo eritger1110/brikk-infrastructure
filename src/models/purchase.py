1417bcf1a0f45ad0898ba0b8198c6f8948b167a1
eritger1110 <eric.ritger@gmail.com>
2025-09-12 14:50:49 -0500

    Refactor import statement and clean up whitespace

--- src/models/purchase.py
+++ src/models/purchase.py
@@ -1,14 +1,14 @@
-from datetime import datetime as dt
+# src/models/purchase.py
+import datetime as dt
 from src.database.db import db
 
 class Purchase(db.Model):
     __tablename__ = "purchases"
-    # Guard in case the module gets imported twice under different names
-    __table_args__ = {"extend_existing": True}
+    __table_args__ = {"extend_existing": True}  # <-- important
 
     id                     = db.Column(db.Integer, primary_key=True)
     order_ref              = db.Column(db.String(32), unique=True, nullable=False, index=True)
     email                  = db.Column(db.String(255), nullable=False, index=True)
-    stripe_customer_id     = db.Column(db.String(64), nullable=True, index=True)
-    stripe_subscription_id = db.Column(db.String(64), nullable=True, index=True)
+    stripe_customer_id     = db.Column(db.String(64),  nullable=True, index=True)
+    stripe_subscription_id = db.Column(db.String(64),  nullable=True, index=True)
     created_at             = db.Column(db.DateTime, nullable=False, default=dt.datetime.utcnow)
