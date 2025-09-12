15424e3a8e1ab7a562278713c6fe3b73f9a05fe4
eritger1110 <eric.ritger@gmail.com>
2025-09-12 12:12:01 -0500

    Remove db initialization from user.py
    Removed unnecessary db initialization from user model.

--- src/models/user.py
+++ src/models/user.py
@@ -5,8 +5,6 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from src.database.db import db
 
-db = SQLAlchemy()
-
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
