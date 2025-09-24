aeafb5a109ab0280bc2b4d084411e93bf5e7c0af
eritger1110 <eric.ritger@gmail.com>
2025-09-23 20:25:27 -0500

    Update __init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,2 +1,10 @@
+# src/models/__init__.py
 # Make models a regular package so modules load once
 from src.database.db import db  # optional convenience re-export
+
+# Export models here so db.create_all() (or any imports) can see them.
+from .agent import Agent          # noqa: F401
+from .customer_profile import CustomerProfile  # if you have/need it  # noqa: F401
+from .purchase import Purchase     # if you have/need it              # noqa: F401
+from .user import User             # if you have/need it              # noqa: F401
+from .audit_log import AuditLog    # NEW                              # noqa: F401
