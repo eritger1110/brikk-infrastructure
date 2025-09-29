7fcea3f373e7b1a603a141171b2efefa739879c6
eritger1110 <eric.ritger@gmail.com>
2025-09-29 14:59:22 -0500

    Update queue.py

--- src/services/queue.py
+++ src/services/queue.py
@@ -3,7 +3,14 @@ import os
 import redis
 from rq import Queue
 
-_redis = redis.from_url(os.environ.get("REDIS_URL") or os.environ.get("RATELIMIT_STORAGE_URI") or "redis://localhost:6379/0")
+redis_url = (
+    os.environ.get("REDIS_URL")
+    or os.environ.get("RATELIMIT_STORAGE_URI")
+    or "redis://localhost:6379/0"
+)
+
+_pool = redis.ConnectionPool.from_url(redis_url, max_connections=10, decode_responses=False)
+_redis = redis.Redis(connection_pool=_pool)
 q = Queue("default", connection=_redis)
 
 def enqueue(func, *args, **kwargs):
