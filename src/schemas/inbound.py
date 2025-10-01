59e45cfa615ffbe3f758cb7f1939999cbdf667ff
eritger1110 <eric.ritger@gmail.com>
2025-10-01 14:23:35 -0500

    Create inbound.py

---
+++ src/schemas/inbound.py
@@ -0,0 +1,9 @@
+# src/schemas/inbound.py
+from marshmallow import Schema, fields
+
+class OrderSchema(Schema):
+    supplier_id = fields.String(required=True)
+    sku         = fields.String(required=True)
+    qty         = fields.Integer(required=True, strict=True)
+    customer_ref = fields.String(load_default=None)
+    idempotency_key = fields.String(load_default=None)
